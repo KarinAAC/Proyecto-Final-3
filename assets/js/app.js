fb7fdac73c5743e4c8e162315c73523edacb1705
KarinAAC <karin_alejo@hotmail.com>
2017-05-02 17:56:27 -0500

    subiendo ultimos detalles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,20 @@
 function abriendoFormulario(){
   spanOculto = document.getElementById("spanOculto");
   spanOculto.style.display="block";
+  var celular = document.getElementById("celular");
+  if( !(/^([9]\d{8})+$/.test(celular.value))){
+    celular.style.border="0.1px solid red";
+  }
+  var nombre = document.getElementById("nombre");
+  if( !(/[a-zA-Z]/.test(nombre.value))){
+    nombre.style.border="0.1px solid red";
+  }
+  var email = document.getElementById("email");
+  if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email.value))){
+    email.style.border="0.1px solid red"
+  }
+  var city = document.getElementById("city");
+  if( !(/[a-zA-Z]/.test(city.value))){
+    city.style.border="0.1px solid red";
+  }
 }
