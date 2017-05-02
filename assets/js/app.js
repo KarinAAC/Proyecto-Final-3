b52af3fcd1d8c57bc99c24f08e1dd1a52a631999
KarinAAC <karin_alejo@hotmail.com>
2017-05-02 18:22:47 -0500

    subiendo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,25 @@
-function abriendoFormulario(){
+function abriendoFormulario(e){
+
   spanOculto = document.getElementById("spanOculto");
   spanOculto.style.display="block";
+  var errorUno = document.getElementById("error-uno");
+  errorUno.style.display="block";
+
   var celular = document.getElementById("celular");
+
   if( !(/^([9]\d{8})+$/.test(celular.value))){
-    celular.style.border="0.1px solid red";
+    celular.style.borderColor="red";
   }
   var nombre = document.getElementById("nombre");
   if( !(/[a-zA-Z]/.test(nombre.value))){
-    nombre.style.border="0.1px solid red";
+    nombre.style.borderColor="red";
   }
   var email = document.getElementById("email");
   if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email.value))){
-    email.style.border="0.1px solid red"
+    email.style.borderColor="red";
   }
   var city = document.getElementById("city");
   if( !(/[a-zA-Z]/.test(city.value))){
-    city.style.border="0.1px solid red";
+    city.style.borderColor="red";
   }
 }
